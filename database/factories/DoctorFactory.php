962b0e4560eeb44fa7f6fe1097cdb4bf1b289440
santiagorivasmolinari <santiago.rivas@lightit.io>
2025-12-15 16:30:59 -0300

    fix: corrects PR changes

--- database/factories/DoctorFactory.php
+++ database/factories/DoctorFactory.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Lightit\Clinics\Domain\Models\Clinic;
 use Lightit\Doctors\Domain\Models\Doctor;
 
 /**
@@ -19,5 +21,16 @@ class DoctorFactory extends Factory
         return [
             'name' => fake()->name()
         ];
+    }
+
+    /**
+     * @param Collection<int, Clinic> $clinics
+     * @return static
+     */
+    public function withClinics(Collection $clinics): static
+    {
+        return $this->afterCreating(function (Doctor $doctor) use ($clinics) {
+            $doctor->clinics()->attach($clinics);
+        });
     }
 }
